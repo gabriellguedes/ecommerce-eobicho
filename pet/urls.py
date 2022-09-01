793d33f1e6d34b891b79e3291bf24db546c88ea4
gabriellguedes <gguedes10@gmail.com>
2022-09-01 17:44:57 -0300

    Update Tuplas Models

--- pet/urls.py
+++ pet/urls.py
@@ -5,11 +5,11 @@ from . import views
 app_name = 'pet'
 
 urlpatterns = [
-    path('', views.form, name="form" ),
-    path('formInspecaoPet/', views.FormInspecaoPet, name='InspecaoPet' ),
-    path('list', views.listPet.as_view(), name="list"),
-    path('update/<int:pk>/', views.updatePet.as_view(), name="update"),
-    path('detail/<int:pk>/', views.detailPet.as_view(), name="detail"),
-    path('delete/<int:pk>/', views.deletePet.as_view(), name="delete"),
+    path('', views.form, name='form' ),
+    path('list', views.listPet.as_view(), name='list'),
+    path('update/<int:pk>/', views.updatePet.as_view(), name='update'),
+    path('detail/<int:pk>/', views.detailPet.as_view(), name='detail'),
+    path('delete/<int:pk>/', views.deletePet.as_view(), name='delete'),
+    path('formInspecaoPet/', views.FormInspecaoPet, name='InspecaoPet'),
         
 ] 
